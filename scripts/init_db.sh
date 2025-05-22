2cc700577ef044c0044aa966ceaad0aee950589f
Mark Milburn <mark.a.milburn@gmail.com>
2025-05-22 16:49:35 -0600

    Add init_db script

---
+++ scripts/init_db.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+
+DB_PORT="${POSTGRES_PORT:=5432}"
+SUPERUSER="${SUPERUSER:=postgres}"
+SUPERUSER_PWD="${SUPERUSER_PWD:=password}"
+
+CONTAINER_NAME="postgres"
+docker run \
+  --env POSTGRES_USER=${SUPERUSER} \
+  --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
+  --publish "${DB_PORT}":5432 \
+  --detach \
+  --name "${CONTAINER_NAME}" \
+  postgres -N 1000
