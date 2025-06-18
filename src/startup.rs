fc50ff04680e37ea4fc7c359bff455000911906c
Mark Milburn <mark.a.milburn@gmail.com>
2025-06-18 16:44:51 -0600

    Update tests to use PgPool
    We have yet to fix failure after successive runs.

--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,17 @@
 use crate::routes;
 use actix_web::dev::Server;
 use actix_web::{App, HttpServer, web};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection_pool);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
+            // Add our postgres connection to the application state.
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
